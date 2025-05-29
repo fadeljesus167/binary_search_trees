e9bbe27c902e3039b920e95de07a3cc203768b9b
Fadel <fadeljesus@gmail.com>
2025-05-29 16:16:57 -0400

    initial commit

---
+++ tree.rb
@@ -0,0 +1,17 @@
+class Node
+  def inititalize
+    @data = nil
+    @left_child = nil
+    @right_child = nil
+  end
+end
+
+class Tree
+  def inititalize
+    @root = Node.new
+  end
+
+  def build_tree(data)
+
+  end
+end
