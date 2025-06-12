06a411a0ac3284377fdb20296257a03088cf19d6
Fadel <fadeljesus@gmail.com>
2025-06-12 10:31:01 -0400

    insert done

--- binary_search.rb
+++ binary_search.rb
@@ -17,20 +17,40 @@ class Tree
   end
 
   def build_tree(data_array)
-    start = 0
-    finale = data_array.length - 1
-    mid = (start + finale)/2
+    return nil if data_array.empty?
 
+    mid = data_array.length/2
     node = Node.new(data_array[mid])
 
-    if data_array.length.eql?(1)
-      return node
-    else
-      node.left_child = build_tree(data_array[start..mid-1])
-      node.right_child = build_tree(data_array[mid+1..finale])
-    end
+    node.left_child = build_tree(data_array[0...mid])
+    node.right_child = build_tree(data_array[mid+1..-1])
   
     return node
+  end
+
+  def insert(new_node)
+    node = @root
+    while !node.nil?
+      if new_node.value <= node.value
+        if node.left_child.nil?
+          node.left_child = new_node
+          break
+        else
+          node = node.left_child
+        end
+      else
+        if node.right_child.nil?
+          node.right_child = new_node
+          break
+        else
+          node = node.right_child
+        end
+      end
+    end
+  end
+
+  def delete(value)
+    
   end
 end
 
@@ -41,5 +61,9 @@ def pretty_print(node = @root, prefix = '', is_left = true)
   puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
   pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
 end
+
+pretty_print(tree.root)
+
+tree.insert(Node.new(-1))
 
 pretty_print(tree.root)
